b0986dbd2e36e0751a3f8408ba4085263db1fd6f
bagyusz <phelyx92@gmail.com>
2014-06-24 22:53:52 +0200

    big update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,14 +41,21 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno320
 BOARD_VENDOR_PLATFORM := viskan
 
 # Architecture
+TARGET_ARCH_VARIANT_CPU := cortex-a9
 TARGET_CPU_VARIANT := krait
 
-# Flags
+# Architecture flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 
-# optimization
-TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET      := true
+TARGET_KRAIT_BIONIC_PLDOFFS   := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH  := 64
+TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 
 # Kernel information
 BOARD_KERNEL_BASE     := 0x80200000
@@ -74,7 +81,6 @@ BOARD_USE_SONY_MACUPDATE := true
 BOARD_HARDWARE_CLASS := device/sony/huashan/cmhw
 
 # Camera
-COMMON_GLOBAL_CFLAGS += -DQCOM_BSP_CAMERA_ABI_HACK
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # GPS
@@ -122,8 +128,11 @@ TARGET_QCOM_MEDIA_VARIANT := caf
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
 TARGET_USES_QCOM_COMPRESSED_AUDIO := true
-TARGET_USES_QCOM_MM_AUDIO := true
-TARGET_LS_USE_ALS_NODE := true
+#BOARD_USES_ALSA_AUDIO := true
+#BOARD_USES_LEGACY_ALSA_AUDIO := true
+#TARGET_USES_QCOM_COMPRESSED_AUDIO := true
+#TARGET_LS_USE_ALS_NODE := true
+#BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
 
 # QCOM enhanced A/V
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
@@ -139,24 +148,32 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
 BOARD_SEPOLICY_DIRS += \
     device/sony/huashan/sepolicy
 
-BOARD_SEPOLICY_UNION := \
-       app.te \
-       bluetooth.te \
-       device.te \
-       domain.te \
-       drmserver.te \
-       file.te \
-       file_contexts \
-       hci_init.te \
-       init_shell.te \
-       keystore.te \
-       mediaserver.te \
-       kickstart.te \
-       netd.te \
-       rild.te \
-       surfaceflinger.te \
-       system.te \
-       ueventd.te \
-       vold.te \
-       wpa.te
+BOARD_SEPOLICY_UNION += \
+    file_contexts \
+    property_contexts \
+    te_macros \
+    bluetooth_loader.te \
+    bridge.te \
+    camera.te \
+    device.te \
+    dhcp.te \
+    domain.te \
+    drmserver.te \
+    file.te \
+    kickstart.te \
+    init.te \
+    mac_update.te \
+    mediaserver.te \
+    mpdecision.te \
+    netmgrd.te \
+    qmux.te \
+    rild.te \
+    rmt.te \
+    surfaceflinger.te \
+    system.te \
+    tee.te \
+    thermald.te \
+    ueventd.te \
+    vold.te \
+    wpa_supplicant.te
 
