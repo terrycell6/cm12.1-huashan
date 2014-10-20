8aee0778b83a0ceb28afbdb15d4dc22e0dfa07db
bagyusz <phelyx92@gmail.com>
2014-10-20 09:14:17 +0200

    Add FM radio support

--- lbhuashan.mk
+++ lbhuashan.mk
@@ -105,6 +105,13 @@ PRODUCT_PACKAGES += \
     libaudio-resampler \
     tinymix
 
+# FM radio
+PRODUCT_PACKAGES += \
+    FM2 \
+    FMRecord \
+    libqcomfm_jni \
+    qcom.fmradio
+
 # BT
 PRODUCT_PACKAGES += \
     hci_qcomm_init
