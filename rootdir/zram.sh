bf8ed30164dcaace06e2fce984335b3f85da3bbf
bagyusz <phelyx92@gmail.com>
2014-10-31 20:58:13 +0100

    update zram.sh zram size 192MB

--- rootdir/zram.sh
+++ rootdir/zram.sh
@@ -2,8 +2,4 @@
 
 insmod /system/lib/modules/zsmalloc.ko
 insmod /system/lib/modules/lzo.ko
-insmod /system/lib/modules/zram.ko
-
-mknod /dev/block/zram0 b 253 0
-
-mkswap /dev/block/zram0
+insmod /system/lib/modules/zram.ko num_devices=1
