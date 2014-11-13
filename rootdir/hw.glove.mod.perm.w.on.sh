9160d60f8a15e9388e76156a9f4f5662b8f3f4e2
bagyusz <phelyx92@gmail.com>
2014-11-13 07:49:00 +0100

    glove mod final fix

--- rootdir/hw.glove.mod.perm.w.on.sh
+++ rootdir/hw.glove.mod.perm.w.on.sh
@@ -28,7 +28,3 @@ fi
 if $mount_needed ;then
   mount -o ro,remount,barrier=1 /system
 fi
-
-sleep 10
-chmod 440 /sys/devices/i2c-3/3-0024/main_ttsp_core.cyttsp4_i2c_adapter/signal_disparity
-chmod 440 /sys/devices/i2c-3/3-0024/main_ttsp_core.cyttsp4_i2c_adapter/finger_threshold
