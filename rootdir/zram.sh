6e317531d5c8ba7fa56c113f428abe12f00a2532
bagyusz <phelyx92@gmail.com>
2014-11-23 21:00:28 +0100

    revert dual core support

--- rootdir/zram.sh
+++ rootdir/zram.sh
@@ -4,15 +4,4 @@ insmod /system/lib/modules/zsmalloc.ko
 sync
 insmod /system/lib/modules/lzo.ko
 sync
-insmod /system/lib/modules/zram.ko num_devices=2
-sync
-echo "268435456" > /sys/block/zram0/disksize
-sync
-echo "268435456" > /sys/block/zram1/disksize
-sync
-mkswap /dev/block/zram0
-sync
-mkswap /dev/block/zram1
-sync
-swapon -p 10 /dev/block/zram0
-swapon -p 10 /dev/block/zram1
+insmod /system/lib/modules/zram.ko num_devices=1
